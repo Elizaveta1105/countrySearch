83a6ea186c41dc9e7ad7875f2f805206c67cc51a
Elizaveta <elizaveta@short.io>
2022-10-03 14:28:51 +0300

    added return

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,7 @@
 
 
 function fetchCountries(value) {
-   
-    fetch(`https://restcountries.com/v3.1/name/${value}?fields=name,capital,flags,population,languages`)
+    return fetch(`https://restcountries.com/v3.1/name/${value}?fields=name,capital,flags,population,languages`)
     .then(response => {
         if(!response.status === "ok") throw new Error("Incorrect requets");
         return response.json()
